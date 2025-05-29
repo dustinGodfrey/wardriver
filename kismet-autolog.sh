9e352cdbf9b53cb289e8b050a1dff347c0703f62
dustinGodfrey | techd47 <44796670+dustinGodfrey@users.noreply.github.com>
2025-05-28 20:58:38 -0500

    Update kismet-autolog.sh

--- kismet-autolog.sh
+++ kismet-autolog.sh
@@ -6,23 +6,23 @@ sudo systemctl stop gpsd.socket
 sudo systemctl disable gpsd.socket
 sleep 1
 sudo gpsd /dev/ttyACM0 -F /var/run/gpsd.sock
-pgrep gpsd || echo "â[!] GPSD failed to start!"
+pgrep gpsd || echo "GPSD failed to start!"
 
-echo "[+] Waiting for GPS fix..."
+echo "Waiting for GPS fix..."
 
-# Wait until GPS reports 3D fix (mode 3)
+# Wait until GPS reports 3D fix
 until gpspipe -w | grep -m1 '"mode":3'; do
-    echo "[+] GPS not fixed yet, waiting..."
+    echo "GPS not fixed yet, waiting..."
     sleep 5
 done
 
-echo "[+] GPS fix acquired, starting Kismet..."
+echo "GPS fix acquired, starting Kismet..."
 touch /tmp/gpsfix
 
 # Generate dated log directory
 LOGDIR="/home/warpig/wardriving/$(date +%Y-%m-%d)/$(date +%H-%M-%S)"
 mkdir -p "$LOGDIR"
 
-# Start Kismet with dynamic log path
-sudo airmon-ng start realtek0
+# Start Kismet
+sudo airmon-ng start wlan1
 exec /usr/local/bin/kismet -c realtek0 --no-nc --log-prefix "$LOGDIR/" 
